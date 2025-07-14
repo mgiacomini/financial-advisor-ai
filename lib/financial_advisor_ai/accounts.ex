58deb57bfc367a862456712dcb50f983ae191dcd
Mauricio Girardello <mauriciogiacomini4@gmail.com>
2025-07-14 13:22:40 -0300

    fix constraint error

--- lib/financial_advisor_ai/accounts.ex
+++ lib/financial_advisor_ai/accounts.ex
@@ -92,7 +92,7 @@ defmodule FinancialAdvisorAi.Accounts do
   def create_oauth_token(attrs) do
     %OAuthToken{}
     |> OAuthToken.changeset(attrs)
-    |> Repo.insert()
+    |> Repo.insert(on_conflict: :replace_all)
   end
 
   def update_oauth_token(token, attrs) do
